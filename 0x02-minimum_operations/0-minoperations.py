fc2da07a8dbce87d228df6c473265654f3055dd7
FTFati <fatima.amkachou@gmail.com>
2024-06-12 14:43:11 -0700

    Min number of operations to reach 4 characters: 4 Min number of operations to reach 12 characters: 7

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+"""Module containing function minoperations"""
+
+
+def minOperations(n):
+    """
+    Calculates the fewest number of operations needed to result
+    in exactly n H characters in the file
+    """
+    if n <= 1:
+        return 0
+
+    operations = 0
+    divisor = 2
+
+    while n > 1:
+        if n % divisor == 0:
+            operations += divisor
+            n //= divisor
+        else:
+            divisor += 1
+
+    return operations
